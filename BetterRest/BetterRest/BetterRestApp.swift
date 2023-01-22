599820e183c5a3195ede725074f1707ee213080d
manny281 <25784989+Manny281@users.noreply.github.com>
2023-01-21 18:10:13 -0600

    day 26

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Manny Martinez on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
