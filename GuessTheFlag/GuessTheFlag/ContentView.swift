b79704572a57172c2881d9b1ff41dcf518a466ab
manny281 <25784989+Manny281@users.noreply.github.com>
2023-01-29 23:19:00 -0600

    day 34

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -25,6 +25,8 @@ struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Monaco", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
 
+    @State private var animationAmount = 0.0
+    @State private var fade = 1.0
     
     var body: some View {
         ZStack {
@@ -55,6 +57,9 @@ struct ContentView: View {
                         } label: {
                             FlagImage(images: countries[number])
                         }
+                        .rotation3DEffect(.degrees(number == self.correctAnswer ? animationAmount : 0), axis: (x: 0, y: 1, z: 0))
+                        .opacity(number == correctAnswer ? 1 : fade)
+                        .scaleEffect(number == correctAnswer ? 1 : fade)
                     }
                 }
                 .frame(maxWidth: .infinity)
@@ -90,6 +95,10 @@ struct ContentView: View {
         if number == correctAnswer {
             scoreTitle = "Correct"
             score += 1
+            withAnimation {
+                animationAmount += 360
+                fade = 0.25
+            }
         } else {
             scoreTitle = "Wrong! That's the flag of \(countries[number])"
         }
@@ -109,11 +118,17 @@ struct ContentView: View {
         counter = 1
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        withAnimation {
+            fade = 1
+        }
     }
     
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        withAnimation {
+            fade = 1
+        }
     }
     
     struct FlagImage: View {
