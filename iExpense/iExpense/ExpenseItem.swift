4bbc1bfc14c506d2b4c86539c063bb596d6273fc
manny281 <25784989+Manny281@users.noreply.github.com>
2023-02-02 18:22:52 -0600

    day 37 app

---
+++ iExpense/iExpense/ExpenseItem.swift
@@ -0,0 +1,15 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Manny Martinez on 2/2/23.
+//
+
+import Foundation
+
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Manny Martinez on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
